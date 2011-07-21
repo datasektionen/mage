1db532f43bdedc555912280b83a595578a6637de
Andreas Tarandi <taran@kth.se>
2011-07-21 14:54:37 +0200

    Adds page structure

--- config/navigation.rb
+++ config/navigation.rb
@@ -53,7 +53,7 @@ SimpleNavigation::Configuration.run do |navigation|
 
     primary.item :reports, 'Rapporter', reports_path
 
-    primary.item :administration, 'Administrera', administration_index_path, :if => Proc.new { current_user.admin? }
+    primary.item :administration, 'Administrera', administration_index_path, :if => Proc.new { current_user and current_user.admin? }
 
     # you can also specify a css id or class to attach to this particular level
     # works for all levels of the menu
