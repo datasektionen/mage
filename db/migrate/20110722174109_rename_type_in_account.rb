d8aef573c3822f90e7eb0c54f84b106e504f2277
Andreas Tarandi <taran@kth.se>
2011-07-22 19:44:03 +0200

    Renaming column
    Type was not an allowed name on a column in rails..

---
+++ db/migrate/20110722174109_rename_type_in_account.rb
@@ -0,0 +1,9 @@
+class RenameTypeInAccount < ActiveRecord::Migration
+  def self.up
+    rename_column :accounts, :type, :account_type
+  end
+
+  def self.down
+    rename_column :accounts, :account_type, :type
+  end
+end
