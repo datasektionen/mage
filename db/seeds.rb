888f15637c8587dbe55492f1f13d18a06d7cd7b4
Andreas Tarandi <taran@kth.se>
2011-07-25 01:50:15 +0200

    Adds content to db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,7 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 #
+ay = ActivityYear.create(:year=>Time.now.year)
+organs = Organ.create([{:name=>"Mottagningen"}, {:name=>"DKM"}, {:name=>"Centralt"}, {:name=>"ESCapo"}, {:name=>"QN"}])
+Serie.create([{:name=>"Centralt", :letter=>"C"}, {:name=>"Mottagningen", :letter=>"M", :default_organ=>Organ.find_by_name("Mottagningen")}, {:name=>"DKM", :letter=>"K", :default_organ=>Organ.find_by_name("DKM")}])
+
