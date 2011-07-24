da098a4e05b8357f443d4e28ddf4001bdda86726
Victor Koronen <koronen@kth.se>
2011-07-24 17:50:29 +0200

    Add tests for Voucher.voucher_rows
    References #15.

--- spec/models/voucher_spec.rb
+++ spec/models/voucher_spec.rb
@@ -1,8 +1,37 @@
 require 'spec_helper'
 
 describe Voucher do
-  it "works" do
+  it "should enforce sum == 0" do
     voucher = Voucher.make
     voucher.should be_valid
+
+    voucher.voucher_rows.first.sum = 100
+    voucher.voucher_rows.last.sum = 200
+
+    voucher.should_not be_valid
+  end
+
+  it "should not allow voucher row destruction" do
+    voucher = Voucher.make
+    voucher_rows = voucher.voucher_rows
+
+    lambda {voucher.voucher_rows.first.destroy}.should raise_error()
+
+    voucher.voucher_rows.should == voucher_rows
+  end
+
+  it "should enforce signatures on added rows" do
+    voucher = Voucher.make
+    voucher.save
+    voucher_rows = voucher.voucher_rows
+
+    sum = rand(100)
+    added_rows = [
+      VoucherRow.make(:voucher => voucher, :sum => sum),
+      VoucherRow.make(:voucher => voucher, :sum => -sum)
+    ]
+    lambda {voucher.voucher_rows << added_rows}.should raise_error()
+
+    voucher.voucher_rows.should == voucher_rows
   end
 end
