7a04b4b9239d9355f35627e0800de0a9f97cf202
Andreas Tarandi <taran@kth.se>
2011-07-28 14:23:09 +0200

    Adds migration for voucher users table

---
+++ db/migrate/20110728121728_create_voucher_users.rb
@@ -0,0 +1,14 @@
+class CreateVoucherUsers < ActiveRecord::Migration
+  def self.up
+    create_table :voucher_users do |t|
+      t.integer user_id, :null=>false
+      t.integer voucher_id, :null=>false
+      t.integer api_key_id, :null=>true
+      t.string role, :null=>false
+    end
+  end
+
+  def self.down
+    drop_table :voucher_users
+  end
+end
