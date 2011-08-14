921af74bfb9aacc72fdfab724600b649322ee444
Andreas Tarandi <torandi@gmail.com>
2011-08-14 15:02:39 +0200

    Adds method for generating api key

--- app/models/api_key.rb
+++ app/models/api_key.rb
@@ -1,3 +1,15 @@
 class ApiKey < ActiveRecord::Base
+  belongs_to :created_by, :class_name => "User"
 
+  validates_presence_of :name, :created_by
+  validates_uniqueness_of :name, :key
+
+  def self.generate_key(name, current_user)
+    api_key = ApiKey.new(:name => name, :created_by => current_user, :key => SecureRandom.hex(32))
+    if api_key.save
+      api_key
+    else
+      nil
+    end
+  end
 end
