cb92a72ef91cdf91d80cdc86850c08cac609371d
Andreas Tarandi <torandi@gmail.com>
2011-08-22 11:56:39 +0200

    Adds template parse code

--- app/models/template.rb
+++ app/models/template.rb
@@ -1,4 +1,36 @@
+#-*- encoding: utf-8 -*-
 class Template < ActiveRecord::Base
-  has_many :template_input_fields
-  has_many :template_output_fields
+  has_many :input_fields, :class_name=>"TemplateInputField"
+  has_many :output_fields, :class_name=>"TemplateOutputField"
+
+  TYPE_MULTIROW = 0
+  TYPE_SINGLEROW = 1
+
+  scope :multirow, where("type=#{TYPE_MULTIROW}")
+  scope :singlerow, where("type=#{TYPE_SINGLEROW}")
+
+  def parse(fields, arr)
+    values = Hash.new
+    fields.each { |k,f| values[k] = f.to_f }
+    result = Array.new
+    to_parse = output_fields.clone
+    last_size = to_parse.size
+    until to_parse.empty?
+      to_parse.each do |f|
+        r = f.parse(values,arr)
+        unless r.nil?
+          to_parse.delete(f)
+          result << r
+          values[f.script_name] = r.sum unless f.script_name.empty?
+        end
+      end
+      if to_parse.size == last_size
+        raise "Kunde inte parsa alla template fält, har du cirkelreferenser?. Följande fält är kvar: #{to_parse.inspect}."
+      else
+        last_size = to_parse.size
+      end
+    end
+    result
+  end
+
 end
--- app/models/template_output_field.rb
+++ app/models/template_output_field.rb
@@ -1,3 +1,27 @@
+#-*- encoding: utf-8 -*-
 class TemplateOutputField < ActiveRecord::Base
-  belongs_to :template
+  belongs_to :template, :inverse_of => :output_fields
+
+  def parse(values,arr) 
+    complete = true
+    _formula = formula.gsub(/\{(.+?)\}/) do |m|
+      if values.key?($1)
+        values[$1]
+      elsif template.output_fields.find_by_script_name($1)
+        # Depends on a not yet parsed output field
+        complete = false
+        m # return the match (don't replace)
+      else
+        raise "Ogiltligt fält i #{script_name} #{account_number}: #{$1}"
+      end
+    end
+    if complete
+      sum = eval(_formula)
+      Rails.logger.debug("#{script_name}: #{_formula} => #{sum}")
+
+      VoucherRow.new(:account_number=>account_number, :arrangement_id => arr, :sum=>sum)
+    else
+      nil
+    end
+  end
 end
