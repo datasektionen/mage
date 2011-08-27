b35e51d632585d70fec56d0042be4db9ad0cb648
Victor Koronen <koronen@kth.se>
2011-08-27 11:51:54 +0200

    Add arrangement report

--- config/navigation.rb
+++ config/navigation.rb
@@ -54,6 +54,7 @@ SimpleNavigation::Configuration.run do |navigation|
     primary.item :reports, 'Rapporter', reports_path do |reports|
       reports.item :all_reports, "Alla rapporter", reports_path
       reports.item :account_report, "Kontorapport", accounts_reports_path, :method => :post
+      reports.item :arrangement_report, "Arrangemangsrapport", arrangements_reports_path, :method => :post
     end
 
     primary.item :administration, 'Administrera', administration_index_path, :if => Proc.new { current_user.admin? } do |admin|
