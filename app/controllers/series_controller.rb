49ebd9cf6aaf51b16ffea36e6a41898b4b718cf1
Andreas Tarandi <torandi@gmail.com>
2011-10-24 19:02:10 +0200

    Fixes problem with serie/series

--- app/controllers/series_controller.rb
+++ app/controllers/series_controller.rb
@@ -1,4 +1,6 @@
 class SeriesController < InheritedResources::Base
+  #Yes, i hate myself, a rewrite serie->series is reeeeally needed
   defaults :resource_class => Serie, :collection_name=>'series', :instance_name => 'serie'
-  load_and_authorize_resource
+  load_and_authorize_resource :serie
+
 end
