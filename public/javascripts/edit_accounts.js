244cdedb4c94076c074150a119f81899a5eb3805
Andreas Tarandi <torandi@gmail.com>
2012-02-10 01:22:23 +0100

    Adds checkboxes in accountplan ui for debet/kredit
    Refs #80

---
+++ public/javascripts/edit_accounts.js
@@ -0,0 +1,19 @@
+$(function() {
+
+$(".update_all_kredit").click(function(e) {
+  var first_val = $(".kredit_is_normal_"+$(this).attr("group_id")).first().prop("checked")
+  if(first_val !== undefined) {
+    $(".kredit_is_normal_"+$(this).attr("group_id")).prop("checked", !first_val);
+  }
+  return false
+})
+  
+$(".update_all_debet").click(function(e) {
+  var first_val = $(".debet_is_normal_"+$(this).attr("group_id")).first().prop("checked")
+  if(first_val !== undefined) {
+    $(".debet_is_normal_"+$(this).attr("group_id")).prop("checked", !first_val);
+  }
+  return false
+})
+
+})
