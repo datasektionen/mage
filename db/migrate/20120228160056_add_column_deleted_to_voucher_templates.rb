e0c4fb2d50fee3ca5ca52376a6f5494dd24ccd2c
Andreas Tarandi <torandi@gmail.com>
2012-02-28 17:15:40 +0100

    Add column for soft delete

---
+++ db/migrate/20120228160056_add_column_deleted_to_voucher_templates.rb
@@ -0,0 +1,9 @@
+class AddColumnDeletedToVoucherTemplates < ActiveRecord::Migration
+  def self.up
+    add_column :voucher_templates, :is_deleted, :bool, :default=>false
+  end
+
+  def self.down
+    remove_column :voucher_templates, :is_deleted
+  end
+end
