526c891cabb5d817ec4e307cbaa014e8b8146513
Andreas Tarandi <torandi@gmail.com>
2012-02-28 18:11:23 +0100

    List accounts correctly for all output fields

--- public/javascripts/voucher_templates.js
+++ public/javascripts/voucher_templates.js
@@ -1,3 +1,14 @@
+$.ui.autocomplete.prototype._renderItem = function(ul,item) {
+    a=item
+    return $("<li></li>")
+      .data("item.autocomplete",a)
+      .append("<a>"+
+               "<span class='autocomplete account'>"+a.number+" - "+a.name+"</span>"+
+              "</a>")
+        .appendTo(ul);
+
+};
+
 $(function() {
 
   $('#voucher_template_valid_from').change(function() {
@@ -18,5 +29,19 @@ $(function() {
 	
   //Update valid_from and to:
   $("#voucher_template_valid_from, #voucher_template_valid_to").trigger('change');
+
+  $(".account_autocomplete:not(.ui-autocomplete-input)").live('focus', function(event) {
+    $(this).autocomplete({
+    source: ac_account,
+    focus: function(event,ui) {
+      $(this).val(ui.item.number);
+      return false;
+    },
+    select: function(event,ui) {
+      $(this).val(ui.item.number);
+      return false;
+    }
+  })
+  })
 
 });
