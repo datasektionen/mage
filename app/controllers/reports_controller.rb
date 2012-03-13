b4061c9bf0963a62b9fa848dd14fef32cb627de5
Andreas Tarandi <torandi@gmail.com>
2012-03-13 22:16:18 +0100

    Add benchmarking

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -5,18 +5,23 @@ class ReportsController < ApplicationController
   end
 
   def show
-    @report_input = params[:report]
-    @report_template = @report_input[:template]
+    self.class.benchmark("input") do 
+      @report_input = params[:report]
+      @report_template = @report_input[:template]
 
-    @series = [Series.find(@report_input[:series])] unless @report_input[:series].empty?
-    @activity_year = ActivityYear.find(@report_input[:activity_year])
-    @organ = @report_input[:organ].empty? ? nil : Organ.find(@report_input[:organ])
-
-    if self.respond_to?(@report_template)
-      self.send(@report_template)
-    else
-      @report = Mage::Report.full_report(@activity_year, @series, @organ)
+      @series = [Series.find(@report_input[:series])] unless @report_input[:series].empty?
+      @activity_year = ActivityYear.find(@report_input[:activity_year])
+      @organ = @report_input[:organ].empty? ? nil : Organ.find(@report_input[:organ])
     end
+
+    self.class.benchmark("build report") do
+      if self.respond_to?(@report_template)
+        self.send(@report_template)
+      else
+        @report = Mage::Report.full_report(@activity_year, @series, @organ)
+      end
+    end
+
     render @report_template
   end 
 end
