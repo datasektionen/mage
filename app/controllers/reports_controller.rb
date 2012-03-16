21da7c01faba61a54a6a4193834b49d67d27dd6b
Andreas Tarandi <torandi@gmail.com>
2012-03-16 22:46:50 +0100

    Fix summarized report

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,25 +1,26 @@
 # -*- encoding: utf-8 -*-
 class ReportsController < ApplicationController
+  @@report_templates = {:complete =>"Huvudbok redovisning", :summary =>"Huvudbok summerad", :accounts => "Kontorapport"}
+
   def index
-    @report_templates = {"Huvudbok"=>:complete, "Huvudbok summerad"=>:summary, "Kontorapport"=>:accounts}
+    @report_templates = @@report_templates
   end
 
   def show
-    self.class.benchmark("input") do 
-      @report_input = params[:report]
-      @report_template = @report_input[:template]
+    @report_input = params[:report]
+    @report_template = @report_input[:template]
+    @report_name = @@report_templates[@report_template.to_sym]
 
-      @series = [Series.find(@report_input[:series])] unless @report_input[:series].empty?
-      @activity_year = ActivityYear.find(@report_input[:activity_year])
-      @organ = @report_input[:organ].empty? ? nil : Organ.find(@report_input[:organ])
-    end
+    @series = [Series.find(@report_input[:series])] unless @report_input[:series].empty?
+    @activity_year = ActivityYear.find(@report_input[:activity_year])
+    @organ = @report_input[:organ].empty? ? nil : Organ.find(@report_input[:organ])
 
-    self.class.benchmark("build report") do
-      if self.respond_to?(@report_template)
-        self.send(@report_template)
-      else
-        @report = Mage::Report.full_report(@activity_year, @series, @organ)
-      end
+    @report_name += " (filtrerad)" if @series || @organ
+
+    if self.respond_to?(@report_template)
+      self.send(@report_template)
+    else
+      @report = Mage::Report.full_report(@activity_year, @series, @organ)
     end
 
     render @report_template
