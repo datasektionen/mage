73d118ecd9293590c69f6eb8e8e0e36da24c3be8
Andreas Tarandi <torandi@gmail.com>
2012-04-13 21:22:32 +0200

    Remove legacy from series read access controll

--- app/controllers/accounting_controller.rb
+++ app/controllers/accounting_controller.rb
@@ -1,10 +1,6 @@
 class AccountingController < ApplicationController
   def index
-    if can? :read, current_series
-      @vouchers = current_activity_year.vouchers.recent(current_series).limit(20)
-    else
-      @vouchers = nil
-    end
+    @vouchers = current_activity_year.vouchers.recent(current_series).limit(20)
     @incomplete_vouchers = current_user.series.map { |s| current_activity_year.vouchers.incomplete.where(:series_id=>s.id) }.flatten
   end
 
