7078e74b35f810f19e6cb2c413b06e7ed17156af
Andreas Tarandi <torandi@gmail.com>
2012-04-13 20:22:19 +0200

    Keep last organ
    Fixes #60

--- app/controllers/vouchers_controller.rb
+++ app/controllers/vouchers_controller.rb
@@ -18,9 +18,17 @@ class VouchersController < InheritedResources::Base
     @voucher.organ = current_series.default_organ || Organ.first
     @voucher.series = current_series
     @voucher.activity_year = current_activity_year
+    last_user_voucher = current_user.vouchers.where(:series_id=>current_series.id, :activity_year_id=>current_activity_year.id).last
     last_voucher = Voucher.where(:series_id=>current_series.id, :activity_year_id=>current_activity_year.id).last
-    @voucher.accounting_date = Time.now 
-    @voucher.accounting_date = last_voucher.accounting_date unless last_voucher.nil?
+    if last_user_voucher
+      @voucher.accounting_date = last_voucher.accounting_date
+      @voucher.organ = last_voucher.organ
+    elsif last_voucher
+      @voucher.accounting_date = last_user_voucher.accounting_date
+      @voucher.organ = last_user_voucher.organ
+    else
+      @voucher.accounting_date = Time.now 
+    end
 
     authorize! :write, @voucher
 
