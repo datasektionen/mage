eb175f79b1a33db5105b598dac76ce6152122b33
Andreas Tarandi <torandi@gmail.com>
2012-04-13 21:57:38 +0200

    Bah, forgot the spec

---
+++ spec/models/activity_year_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe ActivityYear do
+  it "start should be correct" do
+    activity_year = ActivityYear.make
+    activity_year.starts.should == DateTime.new(activity_year.year, 1, 1)
+  end
+
+  it "end should be correct" do
+    activity_year = ActivityYear.make
+    activity_year.ends.should == DateTime.new(activity_year.year, 12, 31)
+  end
+
+  it "find_by_date should return correct activity year" do
+    activity_year = ActivityYear.make
+    activity_year.save
+    ActivityYear.find_by_date(activity_year.starts).year.should == activity_year.year
+    ActivityYear.find_by_date(activity_year.ends).year.should == activity_year.year
+  end
+end
