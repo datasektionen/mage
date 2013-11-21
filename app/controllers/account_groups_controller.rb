575e90ff9b7809d3c1e80ab30e26b1290512fb72
Victor Koronen <koronen@kth.se>
2013-11-21 19:03:01 +0100

    Sort account groups

--- app/controllers/account_groups_controller.rb
+++ app/controllers/account_groups_controller.rb
@@ -1,3 +1,9 @@
 class AccountGroupsController < InheritedResources::Base
   load_and_authorize_resource
+
+  protected
+
+  def collection
+    @account_groups ||= AccountGroup.order(:number)
+  end
 end
